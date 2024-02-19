1e499b0c036fd718f032461b04311ca7ccfe0f3b
HMT <tuan.hm@tinasoft.com.vn>
2024-02-19 17:31:24 +0700

    component-interaction: intercept input property changes with setter

--- component-interaction/src/app/app.module.ts
+++ component-interaction/src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroParentComponent } from './hero-parent.component';
 import { HeroChildComponent } from './hero-child.component';
+import { NameChildComponent } from './name-child.component';
+import { NameParentComponent } from './name-parent.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroParentComponent, HeroChildComponent],
+  declarations: [
+    AppComponent,
+    HeroParentComponent,
+    HeroChildComponent,
+    NameChildComponent,
+    NameParentComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
