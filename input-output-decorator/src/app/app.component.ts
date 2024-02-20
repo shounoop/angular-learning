38e1ab5822f598e04a57653029d481249d99c7ff
HMT <tuan.hm@tinasoft.com.vn>
2024-02-20 10:47:14 +0700

    input-output-decorator: sending data to a parent component

--- input-output-decorator/src/app/app.component.ts
+++ input-output-decorator/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'input-output-decorator';
   currentItem = 'Television';
+  items = ['item1', 'item2', 'item3', 'item4'];
+
+  addItem(newItem: string) {
+    this.items.push(newItem);
+  }
 }
--- input-output-decorator/src/app/app.module.ts
+++ input-output-decorator/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ItemOutputComponent } from './item-output/item-output.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemDetailComponent
+    ItemDetailComponent,
+    ItemOutputComponent
   ],
   imports: [
     BrowserModule,
