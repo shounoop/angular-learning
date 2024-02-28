ac07396453efe87edf7cd7f5c6d3f9c7d84deb76
HMT <tuan.hm@tinasoft.com.vn>
2024-02-28 10:49:15 +0700

    templates: template interpolation

---
+++ template/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  currentCustomer = 'Maria';
+  title = 'title';
+  itemImageUrl =
+    'https://images.unsplash.com/photo-1708831095084-c5b2e2d5e894?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+}
